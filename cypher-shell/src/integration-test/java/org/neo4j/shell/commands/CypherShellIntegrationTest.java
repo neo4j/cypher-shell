3a555524d9b6126e0563faa08b126b8f2ec38dea
Praveena G <praveena.gunasekhar@gmail.com>
2016-07-20 14:59:00 +0100

    integration tests now executes with assertions.

--- cypher-shell/src/integration-test/java/org/neo4j/shell/commands/CypherShellIntegrationTest.java
+++ cypher-shell/src/integration-test/java/org/neo4j/shell/commands/CypherShellIntegrationTest.java
@@ -4,18 +4,23 @@ package org.neo4j.shell.commands;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.neo4j.shell.Command;
 import org.neo4j.shell.ConnectionConfig;
 import org.neo4j.shell.CypherShell;
 import org.neo4j.shell.exception.CommandException;
 import org.neo4j.shell.log.Logger;
-import org.neo4j.shell.log.StdLogger;
+
+import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
 
 public class CypherShellIntegrationTest {
 
-    private Logger logger = new StdLogger();
+    private Logger logger = mock(Logger.class);
     private CypherShell shell = new CypherShell(logger);
     private Command rollbackCommand = new Rollback(shell);
     private Command commitCommand = new Commit(shell);
@@ -28,16 +33,30 @@ public class CypherShellIntegrationTest {
 
     @After
     public void tearDown() throws Exception {
+        shell.execute("MATCH (n:TestPerson) DETACH DELETE (n)");
         shell.disconnect();
     }
 
     @Test
     public void rollbackScenario() throws CommandException {
         try {
+            //given
+            shell.execute("CREATE (:TestPerson {name: \"Jane Smith\"})");
+
+            //when
             beginCommand.execute("");
             shell.execute("CREATE (:Random)");
             rollbackCommand.execute("");
-            shell.execute("MATCH (n) RETURN n");
+
+            //then
+            shell.execute("MATCH (n:TestPerson) RETURN n");
+
+            ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+            verify(logger, times(3)).printOut(captor.capture());
+
+            List<String> result = captor.getAllValues();
+            assertThat(result.get(2), is("n\n{name: \"Jane Smith\"}"));
+
         } catch (CommandException e) {
             assertTrue("unexepcted error", e.getMessage().contains("Not connected"));
         }
@@ -47,8 +66,17 @@ public class CypherShellIntegrationTest {
     public void commitScenario() throws CommandException {
         try {
             beginCommand.execute("");
-            shell.execute("CREATE (:Person {name: \"John Smith\"})");
+            shell.execute("CREATE (:TestPerson {name: \"Joe Smith\"})");
             commitCommand.execute("");
+
+            //then
+            shell.execute("MATCH (n:TestPerson) RETURN n");
+
+            ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+            verify(logger, times(2)).printOut(captor.capture());
+
+            List<String> result = captor.getAllValues();
+            assertThat(result.get(1), is("n\n{name: \"Joe Smith\"}"));
         } catch (CommandException e) {
             assertTrue("unexepcted error", e.getMessage().contains("Not connected"));
         }
