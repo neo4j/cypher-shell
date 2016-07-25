f299ddea24cbfac6e6a5bb6f0916064ac12c4d3f
Jonas Kalderstam <jonas@kalderstam.se>
2016-07-25 12:58:21 +0200

    Expand help tests!

--- cypher-shell/src/test/java/org/neo4j/shell/commands/HelpTest.java
+++ cypher-shell/src/test/java/org/neo4j/shell/commands/HelpTest.java
@@ -6,23 +6,33 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.neo4j.shell.exception.CommandException;
+import org.neo4j.shell.exception.ExitException;
 import org.neo4j.shell.log.Logger;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.TestCase.fail;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.eq;
 
 public class HelpTest {
 
     @Rule
     public final ExpectedException thrown = ExpectedException.none();
-    Logger logger = mock(Logger.class);
+    private Logger logger = mock(Logger.class);
+    private CommandHelper cmdHelper = mock(CommandHelper.class);
 
     private Command cmd;
 
     @Before
     public void setup() {
-        this.cmd = new Help(logger, mock(CommandHelper.class));
+        this.cmd = new Help(logger, cmdHelper);
     }
 
     @Test
@@ -38,5 +48,108 @@ public class HelpTest {
 
         cmd.execute("bob alice");
         fail("Should not accept too many args");
+    }
+
+    @Test
+    public void helpListing() throws CommandException {
+        // given
+        List<Command> commandList = new ArrayList<>();
+
+        commandList.add(new FakeCommand("bob"));
+        commandList.add(new FakeCommand("bobby"));
+
+        doReturn(commandList).when(cmdHelper).getAllCommands();
+
+        // when
+        cmd.execute("");
+
+        // then
+        verify(logger).printOut("\nAvailable commands:");
+        verify(logger).printOut("  @|bold bob  |@ description for bob");
+        verify(logger).printOut("  @|bold bobby|@ description for bobby");
+        verify(logger).printOut("\nFor help on a specific command type:");
+        verify(logger).printOut("    :help @|bold command|@\n");
+        verifyNoMoreInteractions(logger);
+    }
+
+    @Test
+    public void helpForCommand() throws CommandException {
+        // given
+        doReturn(new FakeCommand("bob")).when(cmdHelper).getCommand(eq("bob"));
+
+        // when
+        cmd.execute("bob");
+
+        // then
+        verify(logger).printOut("\nusage: @|bold bob|@ usage for bob\n"
+                               + "\nhelp for bob\n");
+        verifyNoMoreInteractions(logger);
+    }
+
+    @Test
+    public void helpForNonExistingCommandThrows() throws CommandException {
+        // then
+        thrown.expect(CommandException.class);
+        thrown.expectMessage("No such command: @|bold notacommandname|@");
+
+        // when
+        cmd.execute("notacommandname");
+    }
+
+    @Test
+    public void helpForCommandHasOptionalColon() throws CommandException {
+        // given
+        doReturn(new FakeCommand(":bob")).when(cmdHelper).getCommand(eq(":bob"));
+
+        // when
+        cmd.execute("bob");
+
+        // then
+        verify(logger).printOut("\nusage: @|bold :bob|@ usage for :bob\n"
+                + "\nhelp for :bob\n");
+        verifyNoMoreInteractions(logger);
+    }
+
+    private class FakeCommand implements Command {
+        private final String name;
+
+        FakeCommand(String name) {
+            this.name = name;
+        }
+
+        @Nonnull
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Nonnull
+        @Override
+        public String getDescription() {
+            return "description for " + name;
+        }
+
+        @Nonnull
+        @Override
+        public String getUsage() {
+            return "usage for " + name;
+        }
+
+        @Nonnull
+        @Override
+        public String getHelp() {
+            return "help for " + name;
+        }
+
+        @Nonnull
+        @Override
+        public List<String> getAliases() {
+            return new ArrayList<>();
+        }
+
+        @Override
+        public void execute(@Nonnull String args) throws ExitException, CommandException {
+
+        }
     }
 }
