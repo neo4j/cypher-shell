6438a66ce361152ff5fcdc0355f4ef771c6c5b06
Praveena G <praveena.gunasekhar@gmail.com>
2016-09-20 11:34:24 +0100

    pinning java driver to 1.1-20160919.122818-1.jar

--- cypher-shell/src/test/java/org/neo4j/shell/test/bolt/FakeDriver.java
+++ cypher-shell/src/test/java/org/neo4j/shell/test/bolt/FakeDriver.java
@@ -1,5 +1,6 @@
 package org.neo4j.shell.test.bolt;
 
+import org.neo4j.driver.v1.AccessMode;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.exceptions.Neo4jException;
@@ -13,6 +14,11 @@ public class FakeDriver implements Driver {
     @Override
     public Session session() {
         return new FakeSession();
+    }
+
+    @Override
+    public Session session(AccessMode mode) {
+        return null;
     }
 
     @Override
