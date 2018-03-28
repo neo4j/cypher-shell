e871b677d4f20c2f8954f5864b679b09b5a74b31
Praveena G <praveena.gunasekhar@gmail.com>
2018-03-28 11:35:35 +0100

    adding lambda style parameters for param command

--- cypher-shell/src/main/java/org/neo4j/shell/commands/Param.java
+++ cypher-shell/src/main/java/org/neo4j/shell/commands/Param.java
@@ -17,7 +17,10 @@ import java.util.regex.Pattern;
 public class Param implements Command {
     // Match arguments such as "(key) (value with possible spaces)" where key and value are any strings
     private static final Pattern backtickPattern = Pattern.compile("^\\s*(?<key>(`([^`])*`)+?):?\\s+(?<value>.+)$");
+    private static final Pattern backtickLambdaPattern = Pattern.compile("^\\s*(?<key>(`([^`])*`)+?)\\s*=>\\s*(?<value>.+)$");
     private static final Pattern argPattern = Pattern.compile("^\\s*(?<key>[\\p{L}_][\\p{L}0-9_]*):?\\s+(?<value>.+)$");
+    private static final Pattern lambdaPattern = Pattern.compile("^\\s*(?<key>[\\p{L}_][\\p{L}0-9_]*)\\s*=>\\s*(?<value>.+)$");
+    private static final Pattern lambdaMapPattern = Pattern.compile("^\\s*(?<key>[\\p{L}_][\\p{L}0-9_]*):\\s*=>\\s*(?<value>.+)$");
 
     public static final String COMMAND_NAME = ":param";
     private final VariableHolder variableHolder;
@@ -41,7 +44,7 @@ public class Param implements Command {
     @Nonnull
     @Override
     public String getUsage() {
-        return "name value";
+        return "name => value";
     }
 
     @Nonnull
@@ -58,21 +61,32 @@ public class Param implements Command {
 
     @Override
     public void execute(@Nonnull final String argString) throws CommandException {
-        Matcher alphanumericMatcher = argPattern.matcher(argString);
-        if (alphanumericMatcher.matches()) {
-            variableHolder.set(alphanumericMatcher.group("key"), alphanumericMatcher.group("value"));
-        } else {
-            checkForBackticks(argString);
-        }
-    }
-
-    private void checkForBackticks(@Nonnull String argString) throws CommandException {
-        Matcher matcher = backtickPattern.matcher(argString);
-        if (argString.trim().startsWith("`") && matcher.matches() && matcher.group("key").length() > 2) {
-            variableHolder.set(matcher.group("key"), matcher.group("value"));
-        } else {
+        Matcher lambdaMapMatcher = lambdaMapPattern.matcher(argString);
+        if (lambdaMapMatcher.matches()) {
             throw new CommandException(AnsiFormattedText.from("Incorrect number of arguments.\nusage: ")
                     .bold().append(COMMAND_NAME).boldOff().append(" ").append(getUsage()));
         }
+        Matcher lambdaMatcher = lambdaPattern.matcher(argString);
+        if (lambdaMatcher.matches()) {
+            variableHolder.set(lambdaMatcher.group("key"), lambdaMatcher.group("value"));
+            return;
+        }
+        Matcher alphanumericMatcher = argPattern.matcher(argString);
+        if (alphanumericMatcher.matches()) {
+            variableHolder.set(alphanumericMatcher.group("key"), alphanumericMatcher.group("value"));
+            return;
+        }
+        Matcher backtickLambdaMatcher = backtickLambdaPattern.matcher(argString);
+        if (argString.trim().startsWith("`") && backtickLambdaMatcher.matches() && backtickLambdaMatcher.group("key").length() > 2) {
+            variableHolder.set(backtickLambdaMatcher.group("key"), backtickLambdaMatcher.group("value"));
+            return;
+        }
+        Matcher matcher = backtickPattern.matcher(argString);
+        if (argString.trim().startsWith("`") && matcher.matches() && matcher.group("key").length() > 2) {
+            variableHolder.set(matcher.group("key"), matcher.group("value"));
+            return;
+        }
+        throw new CommandException(AnsiFormattedText.from("Incorrect number of arguments.\nusage: ")
+                .bold().append(COMMAND_NAME).boldOff().append(" ").append(getUsage()));
     }
 }
