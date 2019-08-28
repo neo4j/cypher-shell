b92fa65aa30b8674bbb4199cdfaf4976732502c8
Satia Herfert <satiaherfert@gmx.de>
2019-08-28 10:48:57 +0200

    Fix tyre-kicking as well.

--- tyrekicking.sh
+++ tyrekicking.sh
@@ -11,7 +11,10 @@ function prepare-bundle {
 }
 
 function testscript {
-  if cypher-shell/cypher-shell -u neo4j -p neo "RETURN 1;"; then
+  # first try with encryption on (3.X series), if that fails with encryption of (4.X series)
+  if cypher-shell/cypher-shell -u neo4j -p neo --encryption true "RETURN 1;"; then
+    echo "$1 Success!"
+  elif cypher-shell/cypher-shell -u neo4j -p neo --encryption false "RETURN 1;"; then
     echo "$1 Success!"
   else
     echo "$1 Failure!"
