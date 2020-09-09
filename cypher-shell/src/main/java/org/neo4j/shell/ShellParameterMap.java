a20f99119f3467e23c1bdef9a7310ff0996d7495
linneaandersson <Linneamaria10@gmail.com>
2020-09-09 08:38:02 +0200

    Add parboiled fallback when parsing parameters

--- cypher-shell/src/main/java/org/neo4j/shell/ShellParameterMap.java
+++ cypher-shell/src/main/java/org/neo4j/shell/ShellParameterMap.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
 import org.neo4j.cypher.internal.ast.factory.LiteralInterpreter;
+import org.neo4j.cypher.internal.evaluator.EvaluationException;
+import org.neo4j.cypher.internal.evaluator.Evaluator;
+import org.neo4j.cypher.internal.evaluator.ExpressionEvaluator;
 import org.neo4j.cypher.internal.parser.javacc.Cypher;
 import org.neo4j.cypher.internal.parser.javacc.ParseException;
 import org.neo4j.shell.exception.ParameterException;
@@ -20,6 +23,8 @@ public class ShellParameterMap implements ParameterMap
 {
     private final Map<String, ParamValue> queryParams = new HashMap<>();
     private LiteralInterpreter interpreter = new LiteralInterpreter();
+    private ExpressionEvaluator evaluator = Evaluator.expressionEvaluator();
+
 
     @Override
     public Object setParameter( @Nonnull String name, @Nonnull String valueString ) throws ParameterException
@@ -28,11 +33,17 @@ public class ShellParameterMap implements ParameterMap
         try {
             Object value = new Cypher<>( interpreter,
                                          ParameterException.FACTORY,
-                                         new StringReader( valueString ) ).Literal();
+                                         new StringReader( valueString ) ).Expression();
             queryParams.put( parameterName, new ParamValue( valueString, value ) );
             return value;
-        } catch (ParseException e) {
-            throw new ParameterException(e.getMessage());
+        } catch (ParseException | UnsupportedOperationException e) {
+            try {
+                Object value = evaluator.evaluate( valueString, Object.class );
+                queryParams.put( parameterName, new ParamValue( valueString, value ) );
+                return value;
+            } catch (EvaluationException e1) {
+                throw new ParameterException( e1.getMessage() );
+            }
         }
     }
 
